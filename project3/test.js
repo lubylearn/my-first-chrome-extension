b9626d2e5c2f6db0e0afbcf23561d85ab4f1b977
Ejeta Kelvin Emoghene <87129305+lubylearn@users.noreply.github.com>
2023-02-28 20:43:34 +0000

    Add files via upload

---
+++ project3/test.js
@@ -0,0 +1,128 @@
+// addEventlistener =====================================
+
+// let openBox = document.getElementById("open-box")
+// openBox.addEventListener("click", function() {
+//     console.log("i want to open the box")
+// })
+
+// const =================================================
+// const basePrice = 520
+// const discount = 120
+// let shippingCost = 12
+// let shippingTime = "5-12 days"
+
+// shippingCost = 15
+// shippingTime = "5-12 days"
+
+// const fullPrice = basePrice - discount + shippingTime
+
+// console.log("total cost: " + fullPrice + ". it will arrive in " + shippingTime)
+
+// InnerHtml =================================================
+// const buyEL = document.getElementById("buy-el")
+
+// buyEL.innerHTML = "<button>Buy!</button>"
+// buyEL.innerHTML += "<p> thanks for buying</p>"
+
+// const li = document.createElement("li")
+//     li.textContent = myLeads[i]
+//     ulEL.append(li)
+
+// template Strings ==========================================
+
+// const recipient = "james"
+// const sender = "emoghene"
+
+// const email = `hey ${recipient}! 
+// how is it going? 
+// cheers ${sender}`
+
+// console.log(email)
+
+// localStorage ===================================================
+// let name = localStorage.setItem("myName")
+
+// console.log(name)
+
+// localStorage.clear()
+
+// String to Array ======================================================
+
+// let myLeads = `["www.awesomelead.com"]`
+
+// myLeads = JSON.parse(myLeads)
+
+// myLeads.push("www.leads.com")
+
+// myLeads = JSON.stringify(myLeads)
+
+// console.log(typeof myLeads)
+
+// working on Something ===============================================
+
+// const nameEl = document.getElementById("name-el")
+
+// function greetUser(manny, name, emoji) {
+//     nameEl.textContent = `${name}, ${manny} ${emoji}`
+// }
+// greetUser("tommy", "welcome back ", "😁")
+
+// function add(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log( add(3, 4))
+// console.log( add(9, 102))
+
+// function getFirst(arr){
+//     return arr[0]
+// }
+
+// let first = getFirst([10, 2, 5])
+// console.log(first)
+
+
+// need to know ==================================================
+//  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+// console.log(localStorage.getItem("myLeads"))
+
+// i need to work on local storage stuff 
+
+// ======================== Practice Time =========================
+
+// const and let---------------------------------------
+// const player = "Emoghene"
+// const opponent = "Nick"
+// const game = "AmazingFighter"
+
+// let points = 0
+// let hasWon = false
+
+// points += 100
+// hasWon = true
+
+// if (hasWon) {
+//     console.log(`{player} got ${points} points and won the ${game} game`)
+// } else {
+//     console.log("the winner is " + opponent + "! " + player + " lost the game")
+// }
+
+// function -----------------------------------------------------
+
+// let myCourses = ["learn css Animations", "UI Design Fundamentals", "intro to clean code"]
+
+// function courses(arr) {
+//     for (let i = 0; i < arr.length; i++){
+//         console.log(arr[i])
+//     }
+// }
+// courses(myCourses)
+
+// localStorage --------------------------------------------------
+
+// localStorage.setItem("myCredits", "100")
+
+// let myCredits = localStorage.getItem("myCredits")
+// console.log(myCredits)
+
